7ba918b5144bcf542afbdd43e8faef3e1fadacbf
Effy <effy.yin@gmail.com>
2019-08-20 15:09:08 +0800

    modify publicPath

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/comunion/',
+  publicPath: '/',
   outputDir: 'comunion',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
